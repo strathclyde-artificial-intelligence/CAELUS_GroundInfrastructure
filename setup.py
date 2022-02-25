a053a19d6c62e444e4f5f6660b6c0c5121a60bf9
Gianluca Filippi <gian.filippi.91@gmail.com>
2022-02-25 12:51:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='GroundInfrastructure',
+   version='1.0',
+   description='ground infrastructure manager',
+   author='Leonardo Cascianelli',
+   author_email='me.leonardocascianelli@gmail.com',
+   packages=find_packages(),
+   install_requires=['wheel'],
+)
